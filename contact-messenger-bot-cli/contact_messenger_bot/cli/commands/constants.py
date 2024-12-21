133eaf05e18cc2f7b3231b8a611faf4049c5ce8a
Robert Colfin <rcolfin@hotmail.com>
2024-12-20 19:16:11 -0500

    Adding launch scripts' (#7)
    Updating zip_code_cache.json to be consistently named.

--- contact-messenger-bot-cli/contact_messenger_bot/cli/commands/constants.py
+++ contact-messenger-bot-cli/contact_messenger_bot/cli/commands/constants.py
@@ -5,6 +5,6 @@ CREDENTIALS_FILE: Final[Path] = Path(Path.cwd(), "credentials.json").resolve()
 
 TOKEN_FILE: Final[Path] = Path(Path.cwd(), "token.json").resolve()
 
-ZIP_CODE_CACHE_FILE: Final[Path] = Path(Path.cwd(), "zip-code-cache.json").resolve()
+ZIP_CODE_CACHE_FILE: Final[Path] = Path(Path.cwd(), "zip_code_cache.json").resolve()
 
 DATETIME_FMT: Final[str] = "%Y-%m-%d"
---
+++ contact-messenger-bot-functions/scripts/launch.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PACKAGE_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
+
+cd "${PACKAGE_PATH}" || { >&2 echo "Failed to cd to ${PACKAGE_PATH}."; exit 1; }
+
+if [ ! -f credentials.json ] || [ ! -f token.json ] || [ ! -f zip_code_cache.json ]; then
+    echo gcloud storage cp gs://contact-messenger-4ed7624155de0493/*.json .
+    gcloud storage cp gs://contact-messenger-4ed7624155de0493/*.json .
+fi
+
+BEFORE_CREDENTIALS=$(stat --format '%Z' credentials.json)
+BEFORE_TOKEN=$(stat --format '%Z' token.json)
+BEFORE_ZIP_CODE_CACHE=$(stat --format '%Z' zip_code_cache.json)
+
+# shellcheck disable=SC1091
+source "${SCRIPT_PATH}/secrets.sh"
+
+python -m contact_messenger_bot.functions "${@}"
+
+AFTER_CREDENTIALS=$(stat --format '%Z' credentials.json)
+AFTER_TOKEN=$(stat --format '%Z' token.json)
+AFTER_ZIP_CODE_CACHE=$(stat --format '%Z' zip_code_cache.json)
+
+if [ "${AFTER_CREDENTIALS}" \> "${BEFORE_CREDENTIALS}" ]; then
+    echo gcloud storage cp credentials.json gs://contact-messenger-4ed7624155de0493/
+    gcloud storage cp credentials.json gs://contact-messenger-4ed7624155de0493/
+fi
+
+if [ "${AFTER_TOKEN}" \> "${BEFORE_TOKEN}" ]; then
+    echo gcloud storage cp token.json gs://contact-messenger-4ed7624155de0493/
+    gcloud storage cp token.json gs://contact-messenger-4ed7624155de0493/
+fi
+
+if [ "${AFTER_ZIP_CODE_CACHE}" \> "${BEFORE_ZIP_CODE_CACHE}" ]; then
+    echo gcloud storage cp zip_code_cache.json gs://contact-messenger-4ed7624155de0493/
+    gcloud storage cp zip_code_cache.json gs://contact-messenger-4ed7624155de0493/
+fi
+
+cp ./*.json ../contact-messenger-bot-api/
+cp ./*.json ../contact-messenger-bot-cli/
