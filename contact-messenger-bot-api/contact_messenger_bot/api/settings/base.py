3d9172b347af4153a00994b7d4e3d36777c6ecbb
Robert Colfin <robert.m.colfin@gmail.com>
2025-03-26 09:03:08 -0400

    Post uv cleanup

--- contact-messenger-bot-api/contact_messenger_bot/api/settings/base.py
+++ contact-messenger-bot-api/contact_messenger_bot/api/settings/base.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from contact_messenger_bot.api.settings.email import EmailSettings  # noqa: TC001
 from contact_messenger_bot.api.settings.text import TextMessagingSettings  # noqa: TC001
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_nested_delimiter="__")
+
     email: EmailSettings | None = None
     text: TextMessagingSettings | None = None
-
-    class Config:
-        env_nested_delimiter = "__"
 
 
 settings = Settings()
